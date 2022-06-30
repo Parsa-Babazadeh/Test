b33174c6980207d5b4866aab4f66c60310596e07
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2022-06-30 14:51:14 +0430

    2 professional examples for Comparison Operators.

---
+++ Comparison Operators 2.py
@@ -0,0 +1,25 @@
+# if name is less than 3 characters long
+#   name must be at least 3 characters
+# otherwise if it's more than 50 characters long
+#   name can be a maximum of 50 characters
+# otherwise
+#   name looks good!
+
+name = "Parsa"
+
+if len(name) < 3:
+    print("name must be at least 3 characters.")
+elif len(name) > 50:
+    print("name can be a maximum of 50 characters.")
+else:
+    print("name looks good!")
+print(15 * '*')
+#####################################
+name = input("What is your name?: ")
+
+if len(name) < 3:
+    print("name must be at least 3 characters.")
+elif len(name) > 50:
+    print("name can be a maximum of 50 characters.")
+else:
+    print("name looks good!")
