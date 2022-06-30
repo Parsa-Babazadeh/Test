ee8a92dfd10cf471238ecf29dbec6f766d6e9c4d
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2022-06-30 14:14:28 +0430

    2 examples for Comparison Operators

---
+++ Comparison Operators.py
@@ -0,0 +1,23 @@
+# if temperature is greater than 30
+#   it's a hot day
+# otherwise if it's less than 10
+# it's a cold day
+# otherwise
+# it's neither hot nor cold
+
+temperature = 30
+
+if temperature >= 30:
+    print("It's a hot day")
+elif temperature <= 10:
+    print("It's a cold day")
+else:
+    print("It's neither hot nor cold")
+print(15 * "*")
+########################################
+temperature = 20
+
+if temperature >= 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
