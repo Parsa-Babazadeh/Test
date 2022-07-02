a1f276f394115ef8877cd123ead34258ce73be55
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2022-07-02 15:07:40 +0430

    more examples of "For Loops".

---
+++ For Loops 3.py
@@ -0,0 +1,13 @@
+for item in range(10):
+    print(item)
+#########################
+print(15 * "*")
+
+for item in range(5, 10):
+    print(item)
+#########################
+print(15 * "*")
+
+for item in range(5, 10, 2):
+    print(item)
+
